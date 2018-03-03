e9e65f2e10d9818c0029e34f00d947c8f6411867
MarioCat <a.tussy.vassilieff@gmail.com>
2018-03-03 12:36:46 +0100

    Deuxième ajout

---
+++ js/DegRadConv.js
@@ -0,0 +1,14 @@
+// ------------------------- TRANSFO DE DEGRES/RADIANS 
+export function degToRad(deg)
+{
+   return (Math.PI*deg)/180
+}
+// ----------------------------------------------------
+
+// -------------------------- TRANSFO DE DEGRES/RADIANS 
+export function radToDeg(rad)
+{
+   // return (Math.PI*deg)/180
+   return (rad*180)/Math.PI
+}
+// ----------------------------------------------------
